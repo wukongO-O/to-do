75ef0f04013985cefd9e74b4916ee6fd0f86b8de
wukongO-O <l.shenglan@gmail.com>
2022-09-03 14:16:56 -0400

    fix async file img src 404 issue

--- src/projects.js
+++ src/projects.js
@@ -45,7 +45,7 @@ class Project {
         ListsOfTasks.clearTaskDisplay();
         newProjectList.innerHTML = `
             <div class=projectTitle>${projectForm}</div>
-            <button class="addATask"><img src="../dist/add.svg" alt="add project icon"> Add a task</button>
+            <button class="addATask"><svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M22.5 38V25.5H10v-3h12.5V10h3v12.5H38v3H25.5V38Z"/></svg> Add a task</button>
         `
         document.querySelector('.taskList').appendChild(newProjectList);
         
